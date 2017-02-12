5a7010c1c6025eb55b1b0c593db20c55583e53ec
Faissal <glfaissal@gmail.com>
2017-02-12 20:33:20 +0000

    first commit

---
+++ mathlib.py
@@ -0,0 +1,6 @@
+def calc_addition(a, b):
+    return a + b
+
+
+def calc_muliply(a, b):
+    return a * b
---
+++ test_mathlib.py
@@ -0,0 +1,9 @@
+import mathlib
+
+
+def test_calc_addiction():
+    assert mathlib.calc_addition(2, 2) == 4
+
+
+def test_calc_multiply():
+    assert mathlib.calc_muliply(3, 4) == 12
